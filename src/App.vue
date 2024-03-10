671932c7bbd1de6353fd7e925c4a51e510100582
NewBean0312 <newbean0312@gmail.com>
2024-03-10 18:25:13 +0900

    데이터 넣기

--- src/App.vue
+++ src/App.vue
@@ -10,34 +10,46 @@
     <a v-for="(menu, i) in menuData" :key="i">{{ menu }}</a>
   </div>
   <div>
-    <img src="./assets/room0.jpg" class="room-img" />
-    <h4 @click="modalState = true">{{ productData[0] }}</h4>
-    <p>50만원</p>
-    <button @click="increase1">허위매물신고</button>
-    <span> 신고 수 : {{ declareNo[0] }}</span>
+    <img :src="oneRoomData[0].image" class="room-img" />
+    <h4 @click="modalState = true">{{ oneRoomData[0].title }}</h4>
+    <p>{{ oneRoomData[0].price}}</p>
   </div>
   <div>
-    <img src="./assets/room1.jpg" class="room-img" />
-    <h4>{{ productData[1] }}</h4>
-    <p>60만원</p>
-    <button @click="increase2">허위매물신고</button>
-    <span> 신고 수 : {{ declareNo[1] }}</span>
+    <img :src="oneRoomData[1].image" class="room-img" />
+    <h4 @click="modalState = true">{{ oneRoomData[1].title }}</h4>
+    <p>{{ oneRoomData[1].price}}</p>
   </div>
   <div>
-    <img src="./assets/room2.jpg" class="room-img" />
-    <h4>{{ productData[2] }}</h4>
-    <p>70만원</p>
-    <button @click="increase3">허위매물신고</button>
-    <span> 신고 수 : {{ declareNo[2] }}</span>
+    <img :src="oneRoomData[2].image" class="room-img" />
+    <h4 @click="modalState = true">{{ oneRoomData[2].title }}</h4>
+    <p>{{ oneRoomData[2].price}}</p>
+  </div>
+  <div>
+    <img :src="oneRoomData[3].image" class="room-img" />
+    <h4 @click="modalState = true">{{ oneRoomData[3].title }}</h4>
+    <p>{{ oneRoomData[3].price}}</p>
+  </div>
+  <div>
+    <img :src="oneRoomData[4].image" class="room-img" />
+    <h4 @click="modalState = true">{{ oneRoomData[4].title }}</h4>
+    <p>{{ oneRoomData[4].price}}</p>
+  </div>
+  <div>
+    <img :src="oneRoomData[5].image" class="room-img" />
+    <h4 @click="modalState = true">{{ oneRoomData[5].title }}</h4>
+    <p>{{ oneRoomData[5].price}}</p>
   </div>
 </template>
 
 <script>
+import oneRoom from './assets/oneRoom';
+
 export default {
   name: "App",
   data() {
     return {
       // 데이터 보관 (데이터는 object 자료형으로 저장)(state라고도 불림)
+      oneRoomData: oneRoom,
       modalState: false,
       declareNo: [0, 0, 0],
       menuData: ["Home", "Shop", "About"],
@@ -75,7 +87,7 @@ div {
   padding: 20px;
   width: 100%;
   height: 100%;
-  background-color: rgba(0,0,0,0.5);
+  background-color: rgba(0, 0, 0, 0.5);
 }
 
 .white-bg {
